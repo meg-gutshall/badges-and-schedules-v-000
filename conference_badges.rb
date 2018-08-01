2353ab2734caabba4b4b23f869b09ad975ad54cb
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-01 19:48:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,13 +4,16 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+# def batch_badge_creator(attendees)
+#   badges = []
+#   attendees.each do |name|
+#     badges << "Hello, my name is #{name}."
+#   end
+#   badges
+# end
+
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |name|
-    badges << "Hello, my name is #{name}."
-  end
-  badges
-end
+  attendees.map {|name| "Hello, my name is #{name}."}
 
 def assign_rooms(attendees)
   room_assignments = []
